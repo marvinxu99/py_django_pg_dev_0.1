057f6ef8c2bc387bfe5b7087d7f4476469503f31
Marvin Xu <marvinxu99@hotmail.com>
2020-03-11 21:26:25 -0700

    updated deploy to heroku

--- web_project/web_project/settings.py
+++ web_project/web_project/settings.py
@@ -17,7 +17,7 @@ env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-environ.Env.read_env()   # reading .env file
+env.read_env()   # reading .env file
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,10 +82,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = env.str('MY_GMAIL')
 EMAIL_HOST_PASSWORD = env.str('MY_GMAIL_PASSWORD')
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-EMAIL_USE_SSL = True
-EMAIL_PORT = 465
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+# EMAIL_USE_SSL = True
+# EMAIL_PORT = 465
 
 
 # Database
